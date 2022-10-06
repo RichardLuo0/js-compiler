3e6aae95a3a9febbdaf1ebfcbb6b516f3671c293
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2022-10-06 22:41:46 +0800

    test: Fix incorrect regex expression

--- parser-generator/unitTest/Regex.cpp
+++ parser-generator/unitTest/Regex.cpp
@@ -1,13 +1,10 @@
 #include <gtest/gtest.h>
 
-#include <cassert>
-#include <iostream>
-
 #include "Regex.parser.hpp"
 
 using namespace GeneratedParser;
 
 TEST(Regex, String) {
-  Regex regex(R"("([^\\]|(\\"))*")");
-  EXPECT_TRUE(regex.match(R"("aa\"ab")"));
+  Regex regex(R"(/"([^\\]|(\\.))*"/)");
+  EXPECT_TRUE(regex.match(R"("a\"b\c")"));
 }
