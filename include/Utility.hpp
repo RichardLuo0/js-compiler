26c85b67e8a1428aaa58caf5a2e7b3ac10de70ac
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2022-10-19 12:12:25 +0800

    chore: Change const to constexpr in utility

--- include/Utility.hpp
+++ include/Utility.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <cctype>
-#include <functional>
 #include <string_view>
 
 namespace JsCompiler::Utility {
 // Compile-time string hash
-static const unsigned long hashStartNumber = 5381;
-static const unsigned long hashStepNumber = 5U;
+static constexpr unsigned long hashStartNumber = 5381;
+static constexpr unsigned long hashStepNumber = 5U;
 constexpr unsigned long hash(std::string_view str) {
   unsigned long hash = hashStartNumber;
   for (const auto& ch : str) {
