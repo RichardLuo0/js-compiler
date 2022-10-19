1c4b551c459ce793284d6b39f4caa1bcc3743d25
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2022-10-19 09:37:25 +0800

    build: Move gtest cmake into a function

--- cmake/GTest.cmake
+++ cmake/GTest.cmake
@@ -1,15 +1,18 @@
-find_package(GTest)
-if (GTest_FOUND)
-  aux_source_directory(unitTest TEST_SRC)
-  add_executable(${PROJECT_NAME}-test ${TEST_SRC})
-  target_link_libraries(${PROJECT_NAME}-test ${PROJECT_NAME}-lib)
-  target_precompile_headers(${PROJECT_NAME}-test REUSE_FROM ${PROJECT_NAME}-lib)
-  include(GoogleTest)
-  target_link_libraries(
-    ${PROJECT_NAME}-test
-    GTest::gtest_main
-  )
-  gtest_discover_tests(${PROJECT_NAME}-test)
-else()
-  message("Skip testing because GTest is not found")
-endif()
+function(create_gtest dir_name lib_target)
+  find_package(GTest)
+  if (GTest_FOUND)
+    aux_source_directory(${dir_name} TEST_SRC)
+    add_executable(${PROJECT_NAME}-test ${TEST_SRC})
+    target_link_libraries(${PROJECT_NAME}-test ${lib_target})
+    target_include_directories(${PROJECT_NAME}-test PUBLIC $<TARGET_PROPERTY:${lib_target},INCLUDE_DIRECTORIES>)
+    target_precompile_headers(${PROJECT_NAME}-test REUSE_FROM ${lib_target})
+    include(GoogleTest)
+    target_link_libraries(
+      ${PROJECT_NAME}-test
+      GTest::gtest_main
+    )
+    gtest_discover_tests(${PROJECT_NAME}-test)
+  else()
+    message("${PROJECT_NAME} skip testing because GTest is not found")
+  endif()
+endfunction()
