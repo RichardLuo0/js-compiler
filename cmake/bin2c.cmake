3fe5d523d7fe7c0e02c8c60668c752b43b24745b
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2022-10-24 00:58:22 +0800

    build: js.ebnf.o will be rebuild when dependency changes

--- cmake/bin2c.cmake
+++ cmake/bin2c.cmake
@@ -1,4 +1,4 @@
-# https://stackoverflow.com/a/10692876/13287102
+# See https://stackoverflow.com/a/10692876/13287102
 function(bin2c TARGET input_file output_file_name)
   file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${output_file_name}.s "
   .global ${output_file_name}
@@ -8,10 +8,9 @@ ${output_file_name}:
   .incbin \"${input_file}\"
 1:
 ${output_file_name}_size:
-  .int 1b - ${output_file_name}
-  ")
+  .long 1b - ${output_file_name}
+")
+  set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${output_file_name}.s PROPERTY OBJECT_DEPENDS ${input_file})
   set_property(SOURCE ${CMAKE_CURRENT_BINARY_DIR}/${output_file_name}.s PROPERTY LANGUAGE CXX)
   target_sources(${TARGET} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/${output_file_name}.s)
-  add_custom_target(INPUT_FILE DEPENDS ${input_file})
-  add_dependencies(${TARGET} INPUT_FILE)
 endfunction()
